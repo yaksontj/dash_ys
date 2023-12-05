8f379b5ca0c87bb1d5bd445874e7c8e7263e108b
yaksontj <145973504+yaksontj@users.noreply.github.com>
2023-12-05 17:55:30 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,12 +1,7 @@
 import streamlit as st
 import pymysql
 import datetime
-import time
-import os
-import numpy as np
-import math
 import pandas as pd
-import matplotlib.pyplot as plt
 
 # 사이드바에 select box를 활용하여 종을 선택한 다음 그에 해당하는 행만 추출하여 데이터프레임을 만들고자합니다.
 st.sidebar.title('branch')
